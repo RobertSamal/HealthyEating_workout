4231d53f964eb56dc8b3850b48846b82239fee2c
Robert Samal <lodekerobert@gmail.com>
2023-04-28 21:04:42 +0300

    Designed my registration page

---
+++ registration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mystyles/registration.css">
+    <title>Registration Page</title>
+</head>
+<body>
+    
+    <section class="myform">
+        <form action="#" method="post" enctype="multipart/form-data">
+            
+            <input type="text" name="username" id="username" placeholder="Enter Username" required>
+            <input type="email" name="email" id="email" placeholder="Enter Email" required>
+            <input type="password" name="password" id="password" placeholder="Enter Password" required>
+            <label for="profile_pic">Desired Profile Picture:</label>
+            <input type="file" id="profile_pic" name="profile_pic"><br>
+            <button class="contact-btn" type="submit">Register Account</button>
+        </form>
+    </section>
+</body>
+</html>
