5c5c7423d30e311e09481e0e1efb0f93c85b2841
Robert Samal <lodekerobert@gmail.com>
2023-04-28 21:03:47 +0300

    I created a landing page for my system

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mystyles/mycss.css">
+    <title>Grid Css project</title>
+</head>
+<body>
+
+    <div class="mother">
+        <!-- NAVIGATION SECTION -->
+        <nav class="main_nav">
+            
+
+                <div class="navitems">
+                    <a href="registration.php"><button class="btn" title="New user?">Sign Up</button></a>
+                </div>
+
+                <div class="navitems"><img src="images/gymlogo.png" alt="dumbell logo" title="Logo for the system" width="110px"></div>
+
+                <div class="navitems">
+                    <a href="login.php"><button class="btn" title="Already have an account?"> Log in</button></a>
+                </div>
+                
+               
+        </nav>
+        
+        <!-- PHOTO AND PARAGRAPH -->
+    <div class="section2">
+        <div class="picture picture1"><img src="images/workout.jpg" alt="this is an image for 2 ladies working out"
+                width="300px"></div>
+        <div class="paragraph paragraph1">
+            <p><strong>Welcome😍 to the ultimate workout system designed to transform your body and elevate your fitness
+                    journey to the
+                    next
+                    level! Our innovative approach combines science-backed exercises, personalized nutrition plans, and
+                    motivational
+                    support
+                    to help you achieve your fitness goals in the most effective and sustainable way possible. Whether
+                    you're a fitness
+                    enthusiast or a beginner, our system caters to all levels and adapts to your individual needs,
+                    preferences, and
+                    lifestyle. Say goodbye to boring and repetitive workouts, and hello to a fun, challenging, and rewarding
+                    fitness
+                    experience that will keep you coming back for more. Join us today and unleash the athlete within
+                    you</strong></p>
+        </div>
+    
+    </div>
+        
+        <!-- PHOTO GRID SECTION -->
+        <div class="container2">
+
+            <div class="pic pic1"><img src="images/pic1.jpg" alt="" width="300px">
+                <div class="pic-text">Exercise</div>
+            </div>
+
+
+            <div class="pic pic2"><img src="images/pic2.jpg" alt="" width="300px">
+                <div class="pic-text">Hydration</div>
+            </div>
+
+
+            <div class="pic pic3"><img src="images/pic3.jpg" alt="" width="300px">
+                <div class="pic-text">Protein</div>
+            </div>
+
+
+            <div class="pic pic4"><img src="images/pic4.jpg" alt="" width="300px">
+                <div class="pic-text">Stretching</div>
+            </div>
+
+            
+            <div class="pic pic5"><img src="images/pic5.jpg" alt="" width="300px">
+                <div class="pic-text">Cardio</div>
+            </div>
+
+
+            <div class="pic pic6"><img src="images/pic6.jpg" alt="" width="300px">
+                <div class="pic-text">Balance</div>
+            
+            </div>
+
+
+            <div class="pic pic7"><img src="images/pic7.jpg" alt="" width="300px">
+                <div class="pic-text">Nutrition</div>
+            
+            </div>
+
+
+            <div class="pic pic8"><img src="images/pic8.jpg" alt="" width="300px">
+                <div class="pic-text">Motivation</div>
+            </div>
+
+
+            <div class="pic pic9"><img src="images/pic9.jpg" alt="" width="300px">
+                <div class="pic-text">Rest</div>
+            </div>
+        
+        </div>
+
+        <footer class="footer1">
+            &copyAll rights reserved - Robert Samal
+        </footer>
+
+    </div>
+    
+
+
+    
+</body>
+</html>
